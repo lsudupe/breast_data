2ceb3885765589a547d6b0800ea2e436de53e6c1
lsudupe <sudupelaura@gmail.com>
2023-09-03 12:01:50 +0900

    adapt them to this repo

--- bin/launch-train.sh
+++ bin/launch-train.sh
@@ -3,9 +3,11 @@
 # entire script fails if a single command fails
 set -e
 
+# script should be run from the project directory
+PROJECT_DIR="/ibex/scratch/medinils/breast_data"
 
 # creates a separate directory for each job
-JOB_NAME=example-training-job
+JOB_NAME=train-graph-model
 mkdir -p /ibex/scratch/medinils/breast_data/results"$JOB_NAME"
 
 # launch the training job
