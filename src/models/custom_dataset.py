720a1576bae6ef60e95fc1dda996e16d255f83f8
lsudupe <sudupelaura@gmail.com>
2023-09-05 15:51:54 +0900

    small change init

--- src/models/custom_dataset.py
+++ src/models/custom_dataset.py
@@ -1,12 +1,12 @@
 # create a custom dataset class
 
 class BreastData(Dataset):
-    def __int__(self, data_list, root, transform=None, pre_transform=None):
+    def __init__(self, data_list, root, transform=None, pre_transform=None):
         super(BreastData, self).__init__(root, transform, pre_transform)
         self._data_list = data_list
 
     def len(self):
-        return len(self.data_list)
+        return len(self._data_list)
 
     def get(self, idx):
-        return self.data_list[idx]
+        return self._data_list[idx]
