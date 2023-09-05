76f414d2ef144402b37896f9cdc76f33f35d3d14
lsudupe <sudupelaura@gmail.com>
2023-09-05 15:41:53 +0900

    script to class BreasData

---
+++ src/models/custom_dataset.py
@@ -0,0 +1,12 @@
+# create a custom dataset class
+
+class BreastData(Dataset):
+    def __int__(self, data_list, root, transform=None, pre_transform=None):
+        super(BreastData, self).__init__(root, transform, pre_transform)
+        self._data_list = data_list
+
+    def len(self):
+        return len(self._data_list)
+
+    def get(self, idx):
+        return self._data_list[idx]
