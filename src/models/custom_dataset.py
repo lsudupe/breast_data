ad04d1c985d2ef786a504401bf455246e925cbfe
lsudupe <sudupelaura@gmail.com>
2023-09-06 17:01:48 +0900

    change identation

--- src/models/custom_dataset.py
+++ src/models/custom_dataset.py
@@ -1,17 +1,17 @@
 # create a custom dataset class
 
-    class BreastData(Dataset):
-        def __init__(self, data_list, root, transform=None, pre_transform=None):
-            super(BreastData, self).__init__(root, transform, pre_transform)
-            self.data_list = data_list
+class BreastData(Dataset):
+    def __init__(self, data_list, root, transform=None, pre_transform=None):
+        super(BreastData, self).__init__(root, transform, pre_transform)
+        self.data_list = data_list
 
-        def len(self):
-            return len(self.data_list)
+    def len(self):
+        return len(self.data_list)
 
-        def get(self, idx):
-            return self.data_list[idx]
+    def get(self, idx):
+        return self.data_list[idx]
 
-        @property
-        def processed_file_names(self):
-            # This function expects names of processed files, but we can return a dummy list
-            return ['dummy']
+ @property
+    def processed_file_names(self):
+        # This function expects names of processed files, but we can return a dummy list
+        return ['dummy']
