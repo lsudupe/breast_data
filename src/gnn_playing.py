5aa320d63a235b96299530906de83c2a4afedb57
lsudupe <sudupelaura@gmail.com>
2023-09-19 10:42:50 +0900

    change function name GCN to GNN

--- src/gnn_playing.py
+++ src/gnn_playing.py
@@ -86,7 +86,7 @@ for step, data in enumerate(train_loader):
     print()
 
 ## Training GNN
-from src.models.GNN import GCN
+from src.models.GNN import GNN
 
 model = GNN(dataset=datasetLaura, hidden_channels=64)
 print(model)
