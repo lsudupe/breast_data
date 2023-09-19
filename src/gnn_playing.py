7935385e4f5eacd12429d78def629da5e956b95d
lsudupe <sudupelaura@gmail.com>
2023-09-19 13:48:35 +0900

    refactor to add different models

--- src/gnn_playing.py
+++ src/gnn_playing.py
@@ -1,45 +1,18 @@
-import sys
+# Run GNN models
 
+## libraries
+import sys
 sys.path.append('/ibex/scratch/medinils/breast_data/')
 import torch
 import os
-from torch_geometric.utils import from_networkx
-import pickle
 from collections import defaultdict
 from sklearn.model_selection import KFold
 from torch_geometric.loader import DataLoader
 from src.models.custom_dataset import BreastData
 from src.models.GNN import GNN
 
-# Load the dictionary
-save_path = "/ibex/scratch/medinils/breast_data/data/process/graphs.pkl"
-with open(save_path, 'rb') as file:
-    graphs = pickle.load(file)
-
-data_list = []
-for graph in graphs.values():
-    data = from_networkx(graph, group_node_attrs=["features"])
-    data.y = torch.tensor([graph.graph['label']], dtype=torch.long)
-    data_list.append(data)
-
-## create dataset
-datasetLaura = BreastData(data_list=data_list, root="/ibex/scratch/medinils/breast_data/data/process/")
-
-print(f'Dataset: {datasetLaura}:')
-print(f'Number of features: {datasetLaura.num_features}')
-print(f'Number of classes: {datasetLaura.num_classes}')
-
-## check first graph
-data = datasetLaura[0]
-
-print(f'Number of nodes: {data.num_nodes}')
-print(f'Number of edges: {data.num_edges}')
-print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
-print(f'Has isolated nodes: {data.has_isolated_nodes()}')
-print(f'Has self-loops: {data.has_self_loops()}')
-print(f'Is undirected: {data.is_undirected()}')
-
-G1 = next(iter(graphs.values()))
+## read dataset
+datasetLaura = BreastData(root="/ibex/scratch/medinils/breast_data/data/process/")
 
 patient_ids = [data.patient_id for data in datasetLaura]
 sample_ids = [data.sample_id for data in datasetLaura]
@@ -68,7 +41,6 @@ print(f'Number of training graphs: {len(train_dataset)}')
 print(f'Number of test graphs: {len(test_dataset)}')
 
 ## Batching of graphs
-
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
@@ -96,7 +68,6 @@ def train():
         optimizer.step()  # Update parameters based on gradients.
         optimizer.zero_grad()  # Clear gradients.
 
-
 def test(loader):
     model.eval()
 
@@ -108,30 +79,40 @@ def test(loader):
     return correct / len(loader.dataset)  # Derive ratio of correct predictions.
 
 
+
+## lets evaluate the three models
+## list of models
+model_type = ['gcn', 'gat', 'graphsage']
+
 for epoch in range(1, 50):
-    train()
-    train_acc = test(train_loader)
-    test_acc = test(test_loader)
-    print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
+        train()
+        train_acc = test(train_loader)
+        test_acc = test(test_loader)
+        train_accuracies.append(train_acc)
+        test_accuracies.append(test_acc)
+        print(f'Model: {model_type.upper()}, Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
 
-## add epoch plot
-plt.figure(figsize=(10, 6))
-plt.plot(epochs, train_acc, label='Train Accuracy', color='blue', marker='o')
-plt.plot(epochs, test_acc, label='Test Accuracy', color='red', marker='o')
-plt.title('Training and Testing Accuracy Over Epochs')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+    # Save the embeddings
+    embeddings_dir = f"/ibex/scratch/medinils/breast_data/results/{model_type.upper()}/embeddings"
+    if not os.path.exists(embeddings_dir):
+        os.makedirs(embeddings_dir)
 
-## save the plot
-# Specify the directory and filename
-save_dir = "/ibex/scratch/medinils/breast_data/results/GNN"
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
-save_path = os.path.join(save_dir, "accuracy_over_epochs.pdf")
+    torch.save(model.embeddings, os.path.join(embeddings_dir, 'embeddings.pt'))
 
-# Save the figure as a PDF
-plt.savefig(save_path)
+    # Plotting training progress
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, 50), train_accuracies, label='Train Accuracy', color='blue', marker='o')
+    plt.plot(range(1, 50), test_accuracies, label='Test Accuracy', color='red', marker='o')
+    plt.title(f'{model_type.upper()} Training and Testing Accuracy Over Epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    plots_dir = f"/ibex/scratch/medinils/breast_data/results/{model_type.upper()}/plots"
+    if not os.path.exists(plots_dir):
+        os.makedirs(plots_dir)
+
+    plt.savefig(os.path.join(plots_dir, "accuracy_over_epochs.pdf"))
+    plt.show()
