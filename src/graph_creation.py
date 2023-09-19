82f00e590727bd6c6036357fd666980e171a348b
lsudupe <sudupelaura@gmail.com>
2023-09-19 15:21:14 +0900

    take out no using packages

--- src/graph_creation.py
+++ src/graph_creation.py
@@ -4,7 +4,6 @@
 from pathlib import Path
 import numpy as np
 import pandas as pd
-from scipy.spatial.distance import pdist, squareform
 import torch
 from scipy.spatial import Delaunay
 import networkx as nx
